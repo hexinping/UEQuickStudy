f7ea476e594d05ba2fd1007b09c3067dde2d3f0c
hexinping <hexinping@mpjoy.com>
2017-06-30 17:50:54 +0800

    add vs.file and source file

---
+++ MyWarshipGame/Source/MyWarshipGame/MyWarshipGameGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MyWarshipGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYWARSHIPGAME_API AMyWarshipGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
